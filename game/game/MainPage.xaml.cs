8cb10a551ce78952078f58d5267891bf8464f7ae
sunodowafu <hig154era@gmail.com>
2017-07-31 21:03:54 +0300

    add game

---
+++ game/game/MainPage.xaml.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace game
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        int step;
+        int count;
+        int size;
+        Color color;
+        Color colorBorder;
+        Color colorText;
+        Matrix map;
+        Button[,] Qu;
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            step = 0;
+            color = Color.FromArgb(255, 255, 255, 255);
+            colorBorder = Color.FromArgb(255, 122, 122, 122);
+            colorText = Color.FromArgb(255, 0, 0, 0);
+            Message();
+        }
+
+
+
+        public async void Message()
+        {
+            string title = "Выберите размер поля:";
+            ContentDialog message = new ContentDialog();
+            {
+                message.Title = title;
+                message.PrimaryButtonText = "10*10";
+                message.SecondaryButtonText = "15*15";
+            }
+            ContentDialogResult result = await message.ShowAsync();
+            if (result == ContentDialogResult.Primary)
+            {
+                count = 10;
+                size = 10;
+                Qu = new Button[10, 10];
+                Qu = FieldBuilder();
+            }
+            else
+            {
+                count = 15;
+                size = 15;
+                Qu = new Button[15, 15];
+                Qu = FieldBuilder();
+            }
+        }
+
+
+        //create field
+        private Button[,] FieldBuilder()
+        {            
+            Count.Text = count.ToString();
+            Button[,] butQu = new Button[size, size];
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                {
+                    this.CreateQ(i, j, 50, 50);
+                }
+            return butQu;
+        }
+
+        //create the cells
+        private void CreateQ(int i, int j, double width, double height)
+        {
+            Qu[i, j] = new Button();
+            Qu[i, j].Width = width;
+            Qu[i, j].Height = height;
+            Sq.Children.Add(Qu[i, j]);
+            Qu[i, j].Margin = new Thickness(width * j, height * i, 0, 0);
+            Qu[i, j].RightTapped += new RightTappedEventHandler(Button_RightClick);
+            Qu[i, j].Click += new RoutedEventHandler(Button_Click);
+            Qu[i, j].Background = new SolidColorBrush(color);
+            Qu[i, j].BorderBrush = new SolidColorBrush(colorBorder);
+            Qu[i, j].Foreground = new SolidColorBrush(colorText);
+            Qu[i, j].Tag = i.ToString() + " " + j.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            string[] tagBut = new string[2];
+            tagBut = btn.Tag.ToString().Split(' ');
+            int i = Convert.ToInt32(tagBut[0]);
+            int j = Convert.ToInt32(tagBut[1]);
+            if (step == 0)
+            {
+                map = new Matrix(i, j, size, count);
+                OpenQ(i, j);
+            }
+            else
+            {
+                if (map.sqList.Count() == size * size - count) MessageUs("Вы выиграли.");
+                else
+                {
+                    if (map.Ex(i, j) == 1) MessageUs("Вы проиграли.");
+                    else OpenQ(i, j);
+                }
+            }
+        }
+
+        //open the cells
+        private void OpenQ(int i, int j)
+        {
+            step++;
+            map.Square(i, j);
+            foreach (int[] a in map.sqList)
+            {
+                Qu[a[0], a[1]].Content = map.mines[a[0], a[1]].ToString();
+            }
+        }
+
+        //puts the flags
+        private void Button_RightClick(object sender, RightTappedRoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            string[] tagBut = new string[2];
+            tagBut = btn.Tag.ToString().Split(' ');
+            if (!(btn.Content=="1" && btn.Content=="2" && btn.Content == "3" && btn.Content == "4" && btn.Content == "5" && btn.Content == "6" && btn.Content == "7" && btn.Content == "8" && btn.Content == "0"))
+            {
+                btn.Content = "!!!";
+            }
+            count--;
+            Count.Text = count.ToString();
+        }
+
+        //message for user
+        private async void MessageUs(string mes)
+        {
+            ContentDialog messageUs = new ContentDialog();
+            {
+                messageUs.Title = mes;
+                messageUs.PrimaryButtonText = "Начать новую игру.";
+            }
+            ContentDialogResult result = await messageUs.ShowAsync();
+            if (result == ContentDialogResult.Primary)
+            {
+                Frame.Navigate(typeof(game.MainPage));
+            }
+        }
+
+
+    }
+}
---
+++ game/game/Matrix.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game
+{
+    class Matrix
+    {
+        public int[,] mines;
+        private int size;
+        public List<int[]> sqList;
+        private bool[,] open;
+
+        //create a matrix with numbers
+        public Matrix(int x, int y, int n, int count)
+        {
+            int w, v;
+            int[,] a = new int[n + 2, n + 2];
+            this.NullMatrix(a, n + 2);
+            mines = new int[n, n];
+            size = n;
+            sqList = new List<int[]>();
+            open = new bool[n, n];
+            this.NullMatrix(mines, n);
+            Random r = new Random();
+            int i = 0;
+            while (i < count)
+            {
+                w = r.Next(n);
+                v = r.Next(n);
+                if (a[w, v] != 1 && w-1 != x && v-1 != y)
+                {
+                    a[w, v] = 1;
+                    i++;
+                }
+            }
+            for (i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    if (a[i + 1, j + 1] == 1) mines[i, j] = 9;
+                    else mines[i, j] = (a[i + 1 - 1, j + 1 - 1] + a[i + 1, j + 1 - 1] + a[i + 1 + 1, j + 1 - 1]) + (a[i + 1 - 1, j + 1] + a[i + 1, j + 1] + a[i + 1 + 1, j + 1]) + (a[i + 1 - 1, j + 1 + 1] + a[i + 1, j + 1 + 1] + a[i + 1 + 1, j + 1 + 1]);
+                    open[i, j] = false;
+                }
+        }
+
+        public int Ex(int x, int y)
+        {
+            if (mines[x, y] == 9) return 1;
+            else return 0;
+        }
+
+        //create null matrix
+        private void NullMatrix(int[,] a, int n)
+        {
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    a[i, j] = 0;                                   
+        }
+
+        //empty cell
+        private bool empty(int i, int j)
+        {
+            if ((i >= 0) && (i < size))
+            {
+                if ((j >= 0) && (j < size))
+                {
+                    if (mines[i,j] == 0) return true;
+                }
+            }
+            return false;
+        }
+
+        //opens empty cells
+        public void Square(int x, int y)
+        {
+            int[] sq = new int[2];
+            if ((x >= 0) && (x < size) && (y >= 0) && (y < size) && (mines[x, y] != 9))
+            {
+                if (!open[x, y])
+                {
+                    open[x, y] = true;
+                    sq[0] = x;
+                    sq[1] = y;
+                    sqList.Add(sq);
+                    if (mines[x, y] == 0)
+                    {
+                        Square(x - 1, y - 1);
+                        Square(x - 1, y);
+                        Square(x - 1, y + 1);
+                        Square(x, y - 1);
+                        Square(x, y + 1);
+                        Square(x + 1, y - 1);
+                        Square(x + 1, y);
+                        Square(x + 1, y + 1);
+                    }
+                    else
+                    {
+                        if (empty(x - 1, y - 1)) Square(x - 1, y - 1);
+                        if (empty(x - 1, y)) Square(x - 1, y);
+                        if (empty(x - 1, y + 1)) Square(x - 1, y + 1);
+                        if (empty(x, y - 1)) Square(x, y - 1);
+                        if (empty(x, y + 1)) Square(x, y + 1);
+                        if (empty(x + 1, y - 1)) Square(x + 1, y - 1);
+                        if (empty(x + 1, y)) Square(x + 1, y);
+                        if (empty(x + 1, y + 1)) Square(x + 1, y + 1);
+                    }
+                }
+            }
+        }
+    }
+}
